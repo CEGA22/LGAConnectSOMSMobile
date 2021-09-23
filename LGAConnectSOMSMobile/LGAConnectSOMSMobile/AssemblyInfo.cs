d602270df21c17fa7d2d436c99222387ec424361
Ceejay Gabrang <71625955+CEGA22@users.noreply.github.com>
2021-09-23 18:00:42 +0800

    Added Project File

---
+++ LGAConnectSOMSMobile/LGAConnectSOMSMobile/AssemblyInfo.cs
@@ -0,0 +1,40 @@
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+[assembly: ExportFont("Montserrat-Black.ttf", Alias = "MBlack")]
+[assembly: ExportFont("Montserrat-BlackItalic.ttf", Alias = "MBlackItalic")]
+[assembly: ExportFont("Montserrat-Bold.ttf", Alias = "MBold")]
+[assembly: ExportFont("Montserrat-BoldItalic.ttf", Alias = "MBoldItalic")]
+[assembly: ExportFont("Montserrat-ExtraBold.ttf", Alias = "MExtraBold")]
+[assembly: ExportFont("Montserrat-ExtraBoldItalic.ttf", Alias = "MExtraBoldItalic")]
+[assembly: ExportFont("Montserrat-ExtraLight.ttf", Alias = "MExtraLight")]
+[assembly: ExportFont("Montserrat-ExtraLightItalic.ttf", Alias = "MExtraLightItalic")]
+[assembly: ExportFont("Montserrat-Italic.ttf", Alias = "MItalic")]
+[assembly: ExportFont("Montserrat-Light.ttf", Alias = "MLight")]
+[assembly: ExportFont("Montserrat-LightItalic.ttf", Alias = "MLightItalic")]
+[assembly: ExportFont("Montserrat-Medium.ttf", Alias = "MM")]
+[assembly: ExportFont("Montserrat-MediumItalic.ttf", Alias = "MMItalic")]
+[assembly: ExportFont("Montserrat-Regular.ttf", Alias = "MRegular")]
+[assembly: ExportFont("Montserrat-SemiBold.ttf", Alias = "MSemiBold")]
+[assembly: ExportFont("Montserrat-SemiBoldItalic.ttf", Alias = "MSemiBoldItalic")]
+[assembly: ExportFont("Montserrat-Thin.ttf", Alias = "MThin")]
+[assembly: ExportFont("Montserrat-ThinItalic.ttf", Alias = "MThinItalic")]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
